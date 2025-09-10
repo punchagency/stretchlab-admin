faaa672b36d7df58702369a1e735e2b7d38bccc1
Sirlahm <riliwanademola@yahoo.com>
2025-09-10 15:34:28 +0100

    ..

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -58,4 +58,5 @@ export const getTempUserInfo = (): CustomJwtPayload | null => {
 export const deleteUserCookie = (): void => {
   Cookies.remove("token");
   Cookies.remove("temp_token");
+  console.log("deleted user cookie");
 };
